d3c419a7c92511b6bc0b06739095f05a53111c96
antonioqneto <antoniodevneto@gmail.com>
2022-04-03 17:55:50 -0300

    Algumas alterações

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import br.com.dio.desafio.dominio.Curso;
+
 public class Main {
     public static void main(String[] args) {
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Curso sobre Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+
+        curso2.setTitulo("Curso PHP");
+        curso2.setDescricao("Curso sobre PHP");
+        curso2.setCargaHoraria(10);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
 
     }
 }
