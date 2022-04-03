5a69ca2dbdef4e711087ab434e10181a2cb0418b
antonioqneto <antoniodevneto@gmail.com>
2022-04-03 20:13:37 -0300

    Interligando as classes de domínio

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -27,9 +25,33 @@ public class Main {
         mentoria.setDescricao("Mentoria sobre Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp GFT Quality Assurance #1");
+        bootcamp.setDescricao("Conheça já o Bootcamp GFT Quality Assurance #1: um programa feito para você que planeja desenvolver suas habilidades em QA.");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(mentoria);
 
+        Dev dev1 = new Dev();
+        dev1.setNome("Dev1");
+        dev1.inscreverBootcamp(bootcamp);
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos: " + dev1.getConteudosInscritos());
+        dev1.progredir();
+        dev1.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Concluídos: " + dev1.getConteudosConcluidos());
+        System.out.println("XP::" + dev1.calcularTotalXp());
+
+        System.out.println("--------------");
+
+        Dev dev2 = new Dev();
+        dev2.setNome("Dev2");
+        dev2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos: " + dev2.getConteudosInscritos());
+        dev2.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos: " + dev2.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos: " + dev2.getConteudosConcluidos());
+        System.out.println("XP::" + dev2.calcularTotalXp());
     }
 }
